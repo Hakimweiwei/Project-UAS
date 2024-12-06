c463781d4551f0158e214eb464cce546bb3fce92
Hakim <imamspeed243@gmail.com>
2024-12-06 13:08:04 +0700

    first commit

---
+++ UTS - Sewa Iphone/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UTS___Sewa_Iphone
+{
+    public partial class Form1Login : Form
+    {
+        Form2 form2 = new Form2();
+        public Form1Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            form2.ShowDialog();
+            this.Close();
+        }
+
+        private void ExitForm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
